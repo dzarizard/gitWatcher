0f3d99f565bd964c9b80a45a333e429b08fa1a9a
Jarosław Pawlik <jaroslaw.pawlik@comarch.com>
2021-06-02 09:45:36 +0200

    Initial commit

---
+++ gitWatcher/src/main/java/com/dzaro/gitWatcher/GitWatcherApplication.java
@@ -0,0 +1,13 @@
+package com.dzaro.gitWatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitWatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitWatcherApplication.class, args);
+	}
+
+}
---
+++ gitWatcher/src/test/java/com/dzaro/gitWatcher/GitWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzaro.gitWatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitWatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
